2be3967cde21d6fe99bb94e23abec52f1fba0fa9
WinningGamer <160644563+WinningGamer@users.noreply.github.com>
2024-10-21 16:41:02 -0500

    Update LinReg.py

--- LinReg.py
+++ LinReg.py
@@ -1,2 +1,21 @@
 # Plz work
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import datetime
 
+# msft
+df = yf.download("MSFT", period = "1mo")
+# df
+# df['Close'].plot()
+df['Percent_change'] = (df['Close'] - df['Open'])/df['Open'] * 100
+df['Dollar_change'] = (df['Close'] - df['Open'])
+# df['Percent_change'].plot()
+# df = df[['Close','Dollar_change','Percent_change']]
+# df['Forecast']
+#x = df['Date']
+#y1 = df['Close']
+#y2 = df['Dollar_change']
+#plt.plot(x, y1)
+df
